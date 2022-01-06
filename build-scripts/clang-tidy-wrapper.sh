12476751faef3f84a7c0aa6cbbb8dbc0b68a8b2b
Firefox13 <57810301+Firefox13@users.noreply.github.com>
2022-01-06 20:58:50 +0300

    213

--- build-scripts/clang-tidy-wrapper.sh
+++ build-scripts/clang-tidy-wrapper.sh
@@ -6,7 +6,7 @@ set -o pipefail
 plugin=build/tools/clang-tidy-plugin/libCataAnalyzerPlugin.so
 
 set -x
-if [ -f "$plugin" ]
+if [ -f "$plugin" ]23
 then
     LD_PRELOAD=$plugin "$CATA_CLANG_TIDY" "$@"
 else
